f849f0c335d701739983eda7e9035be12c142d79
MarkusIppy <Markus.Ippy1981@gmail.com>
2023-07-16 00:56:53 +0000

    fixed errors

--- updatePowerTune.sh
+++ updatePowerTune.sh
@@ -11,7 +11,9 @@ if [ -d /home/root ]; then
 		else
 		mkdir /home/pi/temp
                 git clone https://github.com/PowerTuneDigital/YoctoExtraPackages.git /home/pi/temp
+                cd /home/pi/temp
                 installopenssl.sh
+                sudo rm -r /home/pi/temp
                 fi
                 sudo rm -r /home/pi/temp
 		if [ -d /home/Recoverysrc ]; then
