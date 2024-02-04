4e7f8d6e1a1ccef64a720558b4a311ac20a097d9
Markus Ippy <info@powertunedigital.com>
2024-02-04 07:49:25 +0100

    revert updatefile

--- updatepi4.sh
+++ updatepi4.sh
@@ -6,8 +6,8 @@ REPO_URL="https://github.com/PowerTuneDigital/YoctoExtraPackages.git"
 # Define the paths for Perl and OpenSSL
 PERL_INSTALL_PATH="/usr/local/lib/perl5/5.38.0"
 OPENSSL_INSTALL_PATH="/usr/local/lib/openssl"
-OPENSSL_VERSION="1.1.1"  # Set the desired OpenSSL version
-OPENSSL_BIN_PATH="/usr/local/bin/openssl${OPENSSL_VERSION}"
+OPENSSL_BIN_PATH="/usr/local/bin/"
+
 
 # Function to check if a command is available
 command_exists() {
@@ -36,22 +36,41 @@ if [ -f "$TMP_DIR/compiled_perl_openssl.tar.gz" ]; then
 
     # Create necessary directories if they don't exist
     mkdir -p "$PERL_INSTALL_PATH"
-    mkdir -p "$OPENSSL_INSTALL_PATH/openssl${OPENSSL_VERSION}"
+    mkdir -p "$OPENSSL_INSTALL_PATH"
     mkdir -p "$OPENSSL_BIN_PATH"
 
     # Copy the compiled Perl and OpenSSL files to the correct paths
     cp -r perl "$PERL_INSTALL_PATH"
-    cp -r openssl "$OPENSSL_INSTALL_PATH/openssl${OPENSSL_VERSION}"
+    cp -r openssl "$OPENSSL_INSTALL_PATH"
     cp openssl/bin/openssl "$OPENSSL_BIN_PATH"
 
     # Register the versions of Perl and OpenSSL
-    echo "export PATH=\"$OPENSSL_BIN_PATH:\$PATH\"" > "/etc/profile.d/yocto_extra_packages_openssl${OPENSSL_VERSION}.sh"
-    echo "export LD_LIBRARY_PATH=\"$OPENSSL_INSTALL_PATH/openssl${OPENSSL_VERSION}/lib:\$LD_LIBRARY_PATH\"" >> "/etc/profile.d/yocto_extra_packages_openssl${OPENSSL_VERSION}.sh"
+    echo "export PATH=\"$OPENSSL_BIN_PATH:\$PATH\"" > /etc/profile.d/yocto_extra_packages.sh
+    echo "export LD_LIBRARY_PATH=\"/usr/local/lib/openssl/openssl/lib:\$LD_LIBRARY_PATH\"" >> /etc/profile.d/yocto_extra_packages.sh
+
+    # Check it the file exists with a incorrect path 
+    FILE="/etc/profile.d/yocto_extra_packages.sh"
+LINE_TO_CHECK='export LD_LIBRARY_PATH="/usr/local/lib/openssl/openssl/openssl/lib:$LD_LIBRARY_PATH"'
+NEW_LINE='export LD_LIBRARY_PATH="/usr/local/lib/openssl/openssl/lib:$LD_LIBRARY_PATH"'
+
+# Check if the file exists
+if [ -f "$FILE" ]; then
+    # Check if the line exists in the file
+    if grep -qF "$LINE_TO_CHECK" "$FILE"; then
+        # Replace the line
+        sed -i "s|$LINE_TO_CHECK|$NEW_LINE|" "$FILE"
+        echo "Line replaced in $FILE"
+    else
+        echo "Line not found in $FILE"
+    fi
+else
+    echo "Error: File $FILE not found."
+fi
 
     # Reload the profile to apply the changes for the current session
-    source "/etc/profile.d/yocto_extra_packages_openssl${OPENSSL_VERSION}.sh"
+    source /etc/profile.d/yocto_extra_packages.sh
 
-    # Inform the user about successful installation
+    # Inform user about successful installation
     echo "Installation completed successfully. Please log out and log back in to apply the changes."
     echo "After logging back in, you can check OpenSSL version with 'openssl version'."
 else
