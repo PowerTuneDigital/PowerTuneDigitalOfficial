eb8fde37ec95ff2c9e24ba2c44b6886723601495
Pierre Grandin <grandinp@gmail.com>
2024-03-26 19:16:58 -0600

    Setting up i18n

---
+++ i18n/t1_de.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>SerialSettings</name>
+    <message>
+        <location filename="SerialSettings.qml" line="57"/>
+        <source>Main</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SerialSettings.qml" line="64"/>
+        <source>Dash Sel.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SerialSettings.qml" line="69"/>
+        <source>Sensehat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SerialSettings.qml" line="73"/>
+        <source>Warn / Gear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SerialSettings.qml" line="77"/>
+        <source>Speedtab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SerialSettings.qml" line="88"/>
+        <source>RPM2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SerialSettings.qml" line="93"/>
+        <source>EX Board</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SerialSettings.qml" line="98"/>
+        <source>Startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SerialSettings.qml" line="103"/>
+        <source>Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SerialSettings.qml" line="109"/>
+        <location filename="SerialSettings.qml" line="112"/>
+        <source>Analog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/t1_sp.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>SerialSettings</name>
+    <message>
+        <source>Main</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dash Sel.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sensehat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warn / Gear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Speedtab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>RPM2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EX Board</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Analog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
