f2cc1859fb2d7cc61b72a8cc5f57f00cdae97021
Pierre Grandin <grandinp@gmail.com>
2024-03-26 19:25:24 -0600

    Added some German translations

--- i18n/t1_de.ts
+++ i18n/t1_de.ts
@@ -1,58 +1,56 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
     <name>SerialSettings</name>
     <message>
-        <location filename="SerialSettings.qml" line="57"/>
+        <location filename="SerialSettings.qml" line="57" />
         <source>Main</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Haupt</translation>
     </message>
     <message>
-        <location filename="SerialSettings.qml" line="64"/>
+        <location filename="SerialSettings.qml" line="64" />
         <source>Dash Sel.</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Dash Ausw.</translation>
     </message>
     <message>
-        <location filename="SerialSettings.qml" line="69"/>
+        <location filename="SerialSettings.qml" line="69" />
         <source>Sensehat</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Sensehat</translation>
     </message>
     <message>
-        <location filename="SerialSettings.qml" line="73"/>
+        <location filename="SerialSettings.qml" line="73" />
         <source>Warn / Gear</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Warn/Gang</translation>
     </message>
     <message>
-        <location filename="SerialSettings.qml" line="77"/>
+        <location filename="SerialSettings.qml" line="77" />
         <source>Speedtab</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Geschw.</translation>
     </message>
     <message>
-        <location filename="SerialSettings.qml" line="88"/>
+        <location filename="SerialSettings.qml" line="88" />
         <source>RPM2</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Drehzahl</translation>
     </message>
     <message>
-        <location filename="SerialSettings.qml" line="93"/>
+        <location filename="SerialSettings.qml" line="93" />
         <source>EX Board</source>
-        <translation type="unfinished"></translation>
+        <translation type="">EX Board</translation>
     </message>
     <message>
-        <location filename="SerialSettings.qml" line="98"/>
+        <location filename="SerialSettings.qml" line="98" />
         <source>Startup</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Aufstart</translation>
     </message>
     <message>
-        <location filename="SerialSettings.qml" line="103"/>
+        <location filename="SerialSettings.qml" line="103" />
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Netzwerk</translation>
     </message>
     <message>
-        <location filename="SerialSettings.qml" line="109"/>
-        <location filename="SerialSettings.qml" line="112"/>
+        <location filename="SerialSettings.qml" line="109" />
+        <location filename="SerialSettings.qml" line="112" />
         <source>Analog</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Analog</translation>
     </message>
 </context>
 </TS>
