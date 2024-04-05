433db20bae65c8848e7d1a6205da0fd21f040199
Pierre Grandin <grandinp@gmail.com>
2024-04-05 15:34:11 -0600

    Make translations list dynamic, and split the monolithic ts file into multiple

--- LanguageManager.cpp
+++ LanguageManager.cpp
@@ -11,7 +11,7 @@ void LanguageManager::changeLanguage(const QString &languageCode)
 {
     qInfo() << "Changing language to" << languageCode;
     qApp->removeTranslator(&m_translator);
-    if(m_translator.load(":/i18n/t1_" + languageCode))
+    if(m_translator.load(":/i18n/" + languageCode))
     {
         qApp->installTranslator(&m_translator);
         m_engine->clearComponentCache(); // Clear the cache to force reloading QML files
