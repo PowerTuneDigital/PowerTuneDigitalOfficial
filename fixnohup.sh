d8eebfc2b2a8987fea8ce7318e205b38ab3c0b73
root <info@powertunedigital.com>
2024-08-15 08:30:24 +0000

    Fix Nohup

--- fixnohup.sh
+++ fixnohup.sh
@@ -4,11 +4,12 @@
 file="/etc/init.d/powertune"
 
 # Search for the specific line containing "nohup ./PowertuneQMLGui -platform eglfs &"
-if grep -q "nohup ./PowertuneQMLGui -platform eglfs &" "$file"; then
+if grep -q "^nohup ./PowertuneQMLGui -platform eglfs &" "$file"; then
     echo "Apply nohup fix"
-    
-    # Replace the line with "./PowertuneQMLGui -platform eglfs &"
-    sed -i 's|nohup ./PowertuneQMLGui -platform eglfs &|./PowertuneQMLGui -platform eglfs &|' "$file"
+
+    # Replace the entire line with "./PowertuneQMLGui -platform eglfs &"
+    # This uses the whole-line match to ensure the entire line is replaced correctly
+    sed -i '/^nohup \.\/PowertuneQMLGui -platform eglfs &$/c\./PowertuneQMLGui -platform eglfs &' "$file"
 else
     echo "Nohup patch not required"
 fi
